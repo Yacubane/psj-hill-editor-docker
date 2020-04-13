0a82a669259754bf5f2966bf99cbb66eb2fda983
jakubdybczak <jakub.dybczak@gmail.com>
2020-04-13 20:26:15 +0200

    Change docker image name

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-docker build -t foo .
+docker build -t psj-hill-editor .
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 rm -rf output
 mkdir output
-docker run -v ${PWD}/hill:/psj-hill:Z -v ${PWD}/output:/psj-output:Z -t foo
+docker run -v ${PWD}/hill:/psj-hill:Z -v ${PWD}/output:/psj-output:Z -t psj-hill-editor
